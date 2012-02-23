414f60ec1621dbb1da256fff20514ce4207b31c9
Sergey Staskov <staskovs@yandex.ru>
2012-02-23 22:03:20 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ableton::Application.config.secret_token = 'e6365fc4a00027712512e756528428e3579a39ad72c093348fe1e2e07b5e90ec25667d9d32b8295b817158c70209b0cfc8b04675f1da841a34c2d3a2d95cad93'
